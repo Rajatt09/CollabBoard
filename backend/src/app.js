921894f05d4902760d1855af091e8f00ef8f8292
Rajatt09 <rajatbhati9559@gmail.com>
2024-06-30 19:30:22 +0530

    handles cors config

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, "https://collab-lime.vercel.app"],
     credentials: true,
   })
 );
